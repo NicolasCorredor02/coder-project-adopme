c83e60c4f48b434e491efe91ca170253c72ea78c
Nicolás Corredor <nicolas.d02@outlook.com>
2025-08-09 23:42:50 -0500

    feat: :sparkles: new urls added and new controller to manage logic of rutes
    new urls added and new controller to manage logic of rutes

--- src/routes/mocks.router.js
+++ src/routes/mocks.router.js
@@ -1,5 +1,10 @@
 import { Router } from 'express';
+import mocksController from '../controllers/mocks.controller.js';
 
 const router = Router();
+
+router.get('/mockingpets', mocksController.createPetsMock);
+router.get('/mockingusers', mocksController.createUsersMock);
+router.post('/generateData', mocksController.generateDataWithMock);
 
 export default router;
